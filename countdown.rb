2a2b8197cc9fdfb1ede6d473f759ee159ddb8dc3
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-05-03 19:45:08 -0700

    commit tree f23d6eac3f238501c6689bd37b3fa8bce7821be1

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown initial
+  while initial > 0
+    puts "#{initial} SECOND(S)!"
+    initial -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+p countdown 10
+
+def countdown_with_sleep initial
+  while initial > 0
+    puts "#{initial} SECOND(S)!"
+    initial -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
+
+p countdown 10
